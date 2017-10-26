ad5350a4f7b903dfc9e38a5f535649f3b6e8797e
jsochoa <jsochoa@ono.com>
2017-10-26 17:44:49 +0200

    otro aportacion

---
+++ prueba/Arreglos.playground/Contents.swift
@@ -0,0 +1,26 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var idiomas = [ "español", "chino", "inglés", "francés"]
+
+idiomas.count
+
+idiomas += ["portugués", "mandarín"]
+
+idiomas[0]
+
+idiomas.count //propiedad
+
+idiomas[5]
+
+idiomas.append("italiano")
+
+let italiano = idiomas.removeLast()
+
+idiomas
+
+idiomas.insert("inglés británico", at: 2)
+
+idiomas.sort()
+
